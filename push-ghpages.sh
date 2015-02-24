5e93f50dc50e24a80128deaf2368259dc512007c
Johnny @PC <johnny.96@gmx.de>
2015-02-24 18:36:19 +0100

    It should work now?

--- push-ghpages.sh
+++ push-ghpages.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-echo Testing
+echo Starting push to gh-pages...
+mkdir deploy
+cd deploy
+
+git config --global user.email "johnny@johnnycrazy.de"
+git config --global user.name "Travis-CI"
+git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/JohnnyCrazy/SpotifyAPI-NET gh-pages > /dev/null
+
+cd gh-pages
+git rm -rf ./
+cp -Rf ../../site ./
+git add -f .
+git commit -m "Automatic built mkdocs | $TRAVIS_BUILD_NUMBER pushed"
+git push -fq origin gh-pages > /dev/null
+
+echo -e "Done"
