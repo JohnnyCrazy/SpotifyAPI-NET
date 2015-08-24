8c1cae9a93d7c2a1e9a0a86f89f2f1ad11bf7882
Jay McLain <jay@quarterspot.com>
2015-08-23 20:50:33 -0500

    Crude example of async call of spotifyapi from MVC controller.

---
+++ SpotifyMVC.Example/Controllers/SpotifyController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using SpotifyAPI.Local;
+
+namespace SpotifyMVC.Example.Controllers
+{
+    public class SpotifyController : Controller
+    {
+        private static readonly SpotifyLocalAPI Spotify;
+
+        static SpotifyController()
+        {
+            Spotify = new SpotifyLocalAPI();
+            Spotify.Connect();
+        }
+
+
+        // GET: Spotify
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> Play()
+        {
+            await Spotify.Play();
+            return View("Index");
+        }
+
+        public async Task<ActionResult> Pause()
+        {
+            await Spotify.Pause();
+            return View("Index");
+        }
+    }
+}
