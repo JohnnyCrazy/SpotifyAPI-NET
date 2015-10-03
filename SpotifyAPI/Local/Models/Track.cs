14b94aebb5eba2837b504a8adfbffd9b1471aad8
Jimmy Appelt <jim.appelt@gmail.com>
2015-10-03 16:16:07 +0200

    Fix crash when playing form related artist tab

--- SpotifyAPI/Local/Models/Track.cs
+++ SpotifyAPI/Local/Models/Track.cs
@@ -41,7 +41,7 @@ namespace SpotifyAPI.Local.Models
         /// <returns>A String, which is the URL to the Albumart</returns>
         public String GetAlbumArtUrl(AlbumArtSize size)
         {
-            if (AlbumResource.Uri == null || AlbumResource.Uri.Contains("local") || AlbumResource.Uri.Contains("spotify:album:0000000000000000000000"))
+            if (AlbumResource.Uri == null || !AlbumResource.Uri.Contains("spotify:album:") || AlbumResource.Uri.Contains("spotify:album:0000000000000000000000"))
                 return "";
 
             int albumsize = 0;
