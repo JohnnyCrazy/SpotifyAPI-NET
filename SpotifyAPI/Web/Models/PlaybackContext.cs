2543ed2f2fc7e8ee7fa1935ca9f8d19952fa62e7
Jonas Dellinger <johnny@johnnycrazy.de>
2017-05-29 23:17:15 +0200

    Added new player endpoints

---
+++ SpotifyAPI/Web/Models/PlaybackContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using SpotifyAPI.Web.Enums;
+
+namespace SpotifyAPI.Web.Models
+{
+    public class PlaybackContext : BasicModel
+    {
+        [JsonProperty("device")]
+        public Device Device { get; set; }
+
+        [JsonProperty("repeat_state")]
+        [JsonConverter(typeof(StringEnumConverter))]
+        public RepeatState RepeatState { get; set; }
+
+        [JsonProperty("shuffle_state")]
+        public bool ShuffleState { get; set; }
+
+        [JsonProperty("context")]
+        public Context Context { get; set; }
+
+        [JsonProperty("timestamp")]
+        public long Timestamp { get; set; }
+
+        [JsonProperty("progress_ms")]
+        public int ProgressMs { get; set; }
+
+        [JsonProperty("is_playing")]
+        public bool IsPlaying { get; set; }
+
+        [JsonProperty("item")]
+        public FullTrack Item { get; set; }
+    }
+}
