6cf77f58a271ea583b196263cdbbc4c3b9f0493e
Jonas Dellinger <johnny@johnnycrazy.de>
2017-07-16 23:30:40 +0200

    Testing build script

--- SpotifyAPI/publish.cmd
+++ SpotifyAPI/publish.cmd
@@ -6,6 +6,7 @@ cd ./SpotifyAPI
 nuget pack ./SpotifyAPI.nuspec -Version %APPVEYOR_REPO_TAG_NAME%
 nuget push ./SpotifyAPI-NET.%APPVEYOR_REPO_TAG_NAME%.nupkg -ApiKey %NUGET_TOKEN%
 cd ../
+
 ) else (
   echo Skipping Publishing
 )
