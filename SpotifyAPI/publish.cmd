06adc23abb40a861e437f7b57a070d3e638e7ae4
Jonas Dellinger <johnny@johnnycrazy.de>
2017-07-16 23:57:02 +0200

    Added source to publish.cmd

--- SpotifyAPI/publish.cmd
+++ SpotifyAPI/publish.cmd
@@ -4,7 +4,7 @@ echo Publishing...
 cd ./SpotifyAPI
 
 nuget pack ./SpotifyAPI.nuspec -Version %APPVEYOR_REPO_TAG_NAME%
-nuget push ./SpotifyAPI-NET.%APPVEYOR_REPO_TAG_NAME%.nupkg -ApiKey %NUGET_TOKEN%
+nuget push ./SpotifyAPI-NET.%APPVEYOR_REPO_TAG_NAME%.nupkg -ApiKey %NUGET_TOKEN% -NonInteractive -Source https://www.nuget.org/api/v2/package
 cd ../
 
 ) else (
