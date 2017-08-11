8fdffa1645dd4a829ef3d34c00e4c7d58efc2dc3
iss0 <jan.steffen@posteo.de>
2017-08-11 23:22:59 +0200

    Remove unnecessary using.

--- SpotifyAPI.Web/SpotifyWebAPI.cs
+++ SpotifyAPI.Web/SpotifyWebAPI.cs
@@ -5,8 +5,6 @@ using SpotifyAPI.Web.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace SpotifyAPI.Web
