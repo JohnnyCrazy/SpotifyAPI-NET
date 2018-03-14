c1e898df114c5caa5b40063a66d9376241e060b3
Alessandro Attard Barbini <11708634+aleab@users.noreply.github.com>
2018-03-14 04:34:15 +0100

    Create ProxyConfig class + Unit tests

---
+++ SpotifyAPI/ProxyConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SpotifyAPI
+{
+    public class ProxyConfig
+    {
+        public string Host { get; set; }
+
+        public int Port { get; set; } = 80;
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Whether to bypass the proxy server for local addresses.
+        /// </summary>
+        public bool BypassProxyOnLocal { get; set; }
+
+        public void Set(ProxyConfig proxyConfig)
+        {
+            this.Host = proxyConfig?.Host;
+            this.Port = proxyConfig?.Port ?? 80;
+            this.Username = proxyConfig?.Username;
+            this.Password = proxyConfig?.Password;
+            this.BypassProxyOnLocal = proxyConfig?.BypassProxyOnLocal ?? false;
+        }
+
+        public Uri GetUri()
+        {
+            UriBuilder uriBuilder = new UriBuilder(Host)
+            {
+                Port = Port
+            };
+            return uriBuilder.Uri;
+        }
+    }
+}
