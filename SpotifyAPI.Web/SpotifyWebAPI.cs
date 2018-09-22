6591b5f64c518ce174db7ab6237565a8584e699a
Philip <23171093+h-philip@users.noreply.github.com>
2018-09-22 17:07:44 +0200

    Changed description of AddPlaylistTrack/Async (#290)
    * Changed description of AddPlaylistTrack/Async
    
    * Changed description of RemovePlaylistTrack/Async

--- SpotifyAPI.Web/SpotifyWebAPI.cs
+++ SpotifyAPI.Web/SpotifyWebAPI.cs
@@ -1573,7 +1573,7 @@ namespace SpotifyAPI.Web
         }
 
         /// <summary>
-        ///     Remove one or more tracks from a user’s playlist.
+        ///     Remove a track from a user’s playlist.
         /// </summary>
         /// <param name="userId">The user's Spotify user ID.</param>
         /// <param name="playlistId">The Spotify ID for the playlist.</param>
@@ -1586,7 +1586,7 @@ namespace SpotifyAPI.Web
         }
 
         /// <summary>
-        ///     Remove one or more tracks from a user’s playlist asynchronously.
+        ///     Remove a track from a user’s playlist asynchronously.
         /// </summary>
         /// <param name="userId">The user's Spotify user ID.</param>
         /// <param name="playlistId">The Spotify ID for the playlist.</param>
@@ -1635,7 +1635,7 @@ namespace SpotifyAPI.Web
         }
 
         /// <summary>
-        ///     Add one or more tracks to a user’s playlist.
+        ///     Add a track to a user’s playlist.
         /// </summary>
         /// <param name="userId">The user's Spotify user ID.</param>
         /// <param name="playlistId">The Spotify ID for the playlist.</param>
@@ -1649,7 +1649,7 @@ namespace SpotifyAPI.Web
         }
 
         /// <summary>
-        ///     Add one or more tracks to a user’s playlist asynchronously.
+        ///     Add a track to a user’s playlist asynchronously.
         /// </summary>
         /// <param name="userId">The user's Spotify user ID.</param>
         /// <param name="playlistId">The Spotify ID for the playlist.</param>
