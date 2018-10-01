ec99296f7d5262e86cd6613f1abbef05de813395
Alessandro Attard Barbini <11708634+aleab@users.noreply.github.com>
2018-10-01 19:53:04 +0200

    Added missing currently_playing_type field to PlaybackContext (#292)

---
+++ SpotifyAPI.Web/Enums/TrackType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SpotifyAPI.Web.Enums
+{
+    [Flags]
+    public enum TrackType
+    {
+        [String("track")]
+        Track = 1,
+
+        [String("episode")]
+        Episode = 2,
+
+        [String("ad")]
+        Ad = 4,
+
+        [String("unknown")]
+        Unknown = 8
+    }
+}
