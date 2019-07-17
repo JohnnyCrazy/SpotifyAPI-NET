0a9499c7d12f4d8cb51839ee5a75960833e7f111
Jonas Dellinger <jonas@dellinger.dev>
2019-07-17 17:26:35 +0200

    Use dotnet pack and update versions accordingly

--- publish.cmd
+++ publish.cmd
@@ -1,14 +1,13 @@
 if "%APPVEYOR_REPO_TAG%" == "true" (
 
 echo Publishing...
-
 cd ./SpotifyAPI.Web
-nuget pack ./SpotifyAPI.Web.nuspec -Version %APPVEYOR_REPO_TAG_NAME%
-nuget push ./SpotifyAPI.Web.%APPVEYOR_REPO_TAG_NAME%.nupkg -ApiKey %NUGET_TOKEN% -NonInteractive -Source https://www.nuget.org/api/v2/package
+dotnet pack -c Release SpotifyAPI.Web.csproj -p:PackageVersion=%APPVEYOR_REPO_TAG_NAME%
+nuget push ./bin/Release/SpotifyAPI.Web.%APPVEYOR_REPO_TAG_NAME%.nupkg -ApiKey %NUGET_TOKEN% -NonInteractive -Source https://www.nuget.org/api/v2/package
 
 cd ../SpotifyAPI.Web.Auth
-nuget pack ./SpotifyAPI.Web.Auth.nuspec -Version %APPVEYOR_REPO_TAG_NAME%
-nuget push ./SpotifyAPI.Web.Auth.%APPVEYOR_REPO_TAG_NAME%.nupkg -ApiKey %NUGET_TOKEN% -NonInteractive -Source https://www.nuget.org/api/v2/package
+dotnet pack -c Release SpotifyAPI.Web.Auth.csproj -p:PackageVersion=%APPVEYOR_REPO_TAG_NAME%
+nuget push ./bin/Release/SpotifyAPI.Web.Auth.%APPVEYOR_REPO_TAG_NAME%.nupkg -ApiKey %NUGET_TOKEN% -NonInteractive -Source https://www.nuget.org/api/v2/package
 
 cd ..
 
