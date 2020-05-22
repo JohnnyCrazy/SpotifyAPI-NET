020b60bec2ee88c89e4b79c4e5a0faed8a346135
Jonas Dellinger <jonas@dellinger.dev>
2020-05-22 13:53:58 +0200

    Added blazor example

--- SpotifyAPI.Web.Examples/Example.ASPBlazor/Program.cs
+++ SpotifyAPI.Web.Examples/Example.ASPBlazor/Program.cs
@@ -1,17 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Example.ASPBlazor
 {
-    public class Program
+  public class Program
     {
         public static void Main(string[] args)
         {
