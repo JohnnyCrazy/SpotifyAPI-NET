3e12f806db8426610773e897224e98533040c8e3
Jonas Dellinger <jonas@dellinger.dev>
2020-06-04 18:34:19 +0200

    Added git config stuff

--- SpotifyAPI.Docs/docs.sh
+++ SpotifyAPI.Docs/docs.sh
@@ -10,6 +10,9 @@ if [ -z "$APPVEYOR_PULL_REQUEST_NUMBER" ]; then
     chmod 700 ~/.ssh
     chmod 600 ~/.ssh/id_rsa
 
+    git config --global user.email "jonas@dellinger.dev"
+    git config --global user.name "AppVeyor Docs Builder"
+
     cd ./SpotifyAPI.Docs
     yarn
     USE_SSH=true GIT_USER=JohnnyCrazy yarn deploy
