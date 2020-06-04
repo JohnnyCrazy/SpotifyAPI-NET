0fe6b44795a766c7054266186605062839151f60
Jonas Dellinger <jonas@dellinger.dev>
2020-06-04 18:18:51 +0200

    Added docs build script

---
+++ SpotifyAPI.Docs/docs.sh
@@ -0,0 +1,12 @@
+if [ -z "$APPVEYOR_PULL_REQUEST_NUMBER" ]; then
+  if [ "$APPVEYOR_REPO_BRANCH" = "master" ]; then
+    echo "Building docs..."
+    nvm alias default 14.3.0
+
+    echo "$GH_DEPLOY_TOKEN" | base64 -d > "$HOME/.ssh/id_rsa"
+
+    cd ./SpotifyAPI.Docs
+    yarn
+    USE_SSH=true GIT_USER=JohnnyCrazy yarn deploy
+  fi
+fi
