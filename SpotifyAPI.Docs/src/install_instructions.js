2d1378c0202471107339d8a317ba92e59ec1283f
Jonas Dellinger <jonas@dellinger.dev>
2020-06-09 03:07:07 +0200

    Updated docs install guide for 6.0.0-beta.3

--- SpotifyAPI.Docs/src/install_instructions.js
+++ SpotifyAPI.Docs/src/install_instructions.js
@@ -4,21 +4,21 @@ import Tabs from '@theme/Tabs'
 import TabItem from '@theme/TabItem'
 
 const installCodeNuget =
-  `Install-Package SpotifyAPI.Web -Version 6.0.0-beta.2
+  `Install-Package SpotifyAPI.Web -Version 6.0.0-beta.3
 # Optional Auth module, which includes an embedded HTTP Server for OAuth2
-Install-Package SpotifyAPI.Web.Auth -Version 6.0.0-beta.2
+Install-Package SpotifyAPI.Web.Auth -Version 6.0.0-beta.3
 `;
 
 const installReference =
-  `<PackageReference Include="SpotifyAPI.Web" Version="6.0.0-beta.2" />
+  `<PackageReference Include="SpotifyAPI.Web" Version="6.0.0-beta.3" />
 <!-- Optional Auth module, which includes an embedded HTTP Server for OAuth2 -->
-<PackageReference Include="SpotifyAPI.Web.Auth" Version="6.0.0-beta.2" />
+<PackageReference Include="SpotifyAPI.Web.Auth" Version="6.0.0-beta.3" />
 `;
 
 const installCodeCLI =
-  `dotnet add package SpotifyAPI.Web --version 6.0.0-beta.2
+  `dotnet add package SpotifyAPI.Web --version 6.0.0-beta.3
 # Optional Auth module, which includes an embedded HTTP Server for OAuth2
-dotnet add package SpotifyAPI.Web.Auth --version 6.0.0-beta.2
+dotnet add package SpotifyAPI.Web.Auth --version 6.0.0-beta.3
 `;
 
 const InstallInstructions = () => {
