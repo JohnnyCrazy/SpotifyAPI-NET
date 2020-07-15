d7538fb23a16b551cbc8ae8ec996ee120fa60682
Jonas Dellinger <jonas@dellinger.dev>
2020-07-15 21:11:40 +0200

    Added Description and Followers to FullPlaylist, fixes #484

--- SpotifyAPI.Web/Models/Response/FullPlaylist.cs
+++ SpotifyAPI.Web/Models/Response/FullPlaylist.cs
@@ -4,7 +4,9 @@ namespace SpotifyAPI.Web
   public class FullPlaylist
   {
     public bool? Collaborative { get; set; }
+    public string? Description { get; set; } = default!;
     public Dictionary<string, string>? ExternalUrls { get; set; } = default!;
+    public Followers Followers { get; set; } = default!;
     public string? Href { get; set; } = default!;
     public string? Id { get; set; } = default!;
     public List<Image>? Images { get; set; } = default!;
