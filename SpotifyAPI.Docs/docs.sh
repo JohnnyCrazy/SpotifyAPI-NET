b14fa9547f10d0c6360d85688db3e8d00dd34a4e
Jonas Dellinger <jonas@dellinger.dev>
2020-11-14 09:40:07 +0100

    Added first workflows for building docs and ci/cd

--- SpotifyAPI.Docs/docs.sh
+++ SpotifyAPI.Docs/docs.sh
@@ -2,19 +2,15 @@
 
 set -e
 
-if [ -z "$APPVEYOR_PULL_REQUEST_NUMBER" ]; then
-  if [ "$APPVEYOR_REPO_BRANCH" = "master" ]; then
-    echo "Building docs..."
+echo "Building docs..."
 
-    echo "$GH_DEPLOY_TOKEN" | base64 -d > "$HOME/.ssh/id_rsa"
-    chmod 700 ~/.ssh
-    chmod 600 ~/.ssh/id_rsa
+echo "$GH_DEPLOY_TOKEN" | base64 -d > "$HOME/.ssh/id_rsa"
+chmod 700 ~/.ssh
+chmod 600 ~/.ssh/id_rsa
 
-    git config --global user.email "jonas@dellinger.dev"
-    git config --global user.name "AppVeyor Docs Builder"
+git config --global user.email "jonas@dellinger.dev"
+git config --global user.name "GH Actions Docs Builder"
 
-    cd ./SpotifyAPI.Docs
-    yarn
-    USE_SSH=true GIT_USER=JohnnyCrazy yarn deploy
-  fi
-fi
+cd ./SpotifyAPI.Docs
+yarn
+USE_SSH=true GIT_USER=JohnnyCrazy yarn deploy
--- publish.sh
+++ publish.sh
@@ -2,25 +2,23 @@
 
 set -e
 
-if [ "$APPVEYOR_REPO_TAG" = "true" ]; then
-  echo "Publishing..."
+echo "Publishing..."
 
-  sudo curl -o /usr/local/bin/nuget.exe https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
-  alias nuget="mono /usr/local/bin/nuget.exe"
+sudo curl -o /usr/local/bin/nuget.exe https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
+alias nuget="mono /usr/local/bin/nuget.exe"
 
-  cd ./SpotifyAPI.Web
-  dotnet pack -c Release SpotifyAPI.Web.csproj -p:PackageVersion="$APPVEYOR_REPO_TAG_NAME"
-  nuget push "./bin/Release/SpotifyAPI.Web.$APPVEYOR_REPO_TAG_NAME.nupkg"\
-    -ApiKey "$NUGET_TOKEN"\
-    -NonInteractive\
-    -Source https://www.nuget.org/api/v2/package
+cd ./SpotifyAPI.Web
+dotnet pack -c Release SpotifyAPI.Web.csproj -p:PackageVersion="$APPVEYOR_REPO_TAG_NAME"
+nuget push "./bin/Release/SpotifyAPI.Web.$APPVEYOR_REPO_TAG_NAME.nupkg"\
+  -ApiKey "$NUGET_TOKEN"\
+  -NonInteractive\
+  -Source https://www.nuget.org/api/v2/package
 
-  cd ../SpotifyAPI.Web.Auth
-  dotnet pack -c Release SpotifyAPI.Web.Auth.csproj -p:PackageVersion="$APPVEYOR_REPO_TAG_NAME"
-  nuget push "./bin/Release/SpotifyAPI.Web.Auth.$APPVEYOR_REPO_TAG_NAME.nupkg"\
-    -ApiKey "$NUGET_TOKEN"\
-    -NonInteractive\
-    -Source https://www.nuget.org/api/v2/package
+cd ../SpotifyAPI.Web.Auth
+dotnet pack -c Release SpotifyAPI.Web.Auth.csproj -p:PackageVersion="$APPVEYOR_REPO_TAG_NAME"
+nuget push "./bin/Release/SpotifyAPI.Web.Auth.$APPVEYOR_REPO_TAG_NAME.nupkg"\
+  -ApiKey "$NUGET_TOKEN"\
+  -NonInteractive\
+  -Source https://www.nuget.org/api/v2/package
 
-  cd ..
-fi
+cd ..
