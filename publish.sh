adfc64b957a1173230aaa027ec4bf6a0071d263d
Jonas Dellinger <jonas@dellinger.dev>
2020-11-14 10:21:38 +0100

    Added publishing to CI/CD if tag is present

--- publish.sh
+++ publish.sh
@@ -4,19 +4,16 @@ set -e
 
 echo "Publishing..."
 
-sudo curl -o /usr/local/bin/nuget.exe https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
-alias nuget="mono /usr/local/bin/nuget.exe"
-
 cd ./SpotifyAPI.Web
-dotnet pack -c Release SpotifyAPI.Web.csproj -p:PackageVersion="$APPVEYOR_REPO_TAG_NAME"
-nuget push "./bin/Release/SpotifyAPI.Web.$APPVEYOR_REPO_TAG_NAME.nupkg"\
+dotnet pack -c Release SpotifyAPI.Web.csproj -p:PackageVersion="$RELEASE_VERSION"
+nuget push "./bin/Release/SpotifyAPI.Web.$RELEASE_VERSION.nupkg"\
   -ApiKey "$NUGET_TOKEN"\
   -NonInteractive\
   -Source https://www.nuget.org/api/v2/package
 
 cd ../SpotifyAPI.Web.Auth
-dotnet pack -c Release SpotifyAPI.Web.Auth.csproj -p:PackageVersion="$APPVEYOR_REPO_TAG_NAME"
-nuget push "./bin/Release/SpotifyAPI.Web.Auth.$APPVEYOR_REPO_TAG_NAME.nupkg"\
+dotnet pack -c Release SpotifyAPI.Web.Auth.csproj -p:PackageVersion="$RELEASE_VERSION"
+nuget push "./bin/Release/SpotifyAPI.Web.Auth.$RELEASE_VERSION.nupkg"\
   -ApiKey "$NUGET_TOKEN"\
   -NonInteractive\
   -Source https://www.nuget.org/api/v2/package
