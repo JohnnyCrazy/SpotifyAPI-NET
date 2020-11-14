35dbc7f57d49c5c626367caa7ae508b0d0d6e044
Jonas Dellinger <jonas@dellinger.dev>
2020-11-14 11:14:24 +0100

    Use webfactory/ssh-agent for the SSH setup

--- SpotifyAPI.Docs/docs.sh
+++ SpotifyAPI.Docs/docs.sh
@@ -4,10 +4,6 @@ set -e
 
 echo "Building docs..."
 
-echo "$GH_DEPLOY_TOKEN" | base64 -d > "$HOME/.ssh/id_rsa"
-chmod 700 ~/.ssh
-chmod 600 ~/.ssh/id_rsa
-
 git config --global user.email "jonas@dellinger.dev"
 git config --global user.name "GH Actions Docs Builder"
 
