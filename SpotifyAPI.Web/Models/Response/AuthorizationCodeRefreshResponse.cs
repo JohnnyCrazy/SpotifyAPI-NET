9ca573f74810dc0277dff5a6cf734e3f26406d75
Jonas Dellinger <jonas@dellinger.dev>
2021-08-20 16:11:19 +0200

    Add RefreshToken to AuthorizationCodeRefreshResponse

--- SpotifyAPI.Web/Models/Response/AuthorizationCodeRefreshResponse.cs
+++ SpotifyAPI.Web/Models/Response/AuthorizationCodeRefreshResponse.cs
@@ -2,12 +2,13 @@ using System;
 
 namespace SpotifyAPI.Web
 {
-  public class AuthorizationCodeRefreshResponse: IUserToken
+  public class AuthorizationCodeRefreshResponse: IRefreshableToken
   {
     public string AccessToken { get; set; } = default!;
     public string TokenType { get; set; } = default!;
     public int ExpiresIn { get; set; }
     public string Scope { get; set; } = default!;
+    public string RefreshToken { get; set; } = default!;
 
     /// <summary>
     ///   Auto-Initalized to UTC Now
