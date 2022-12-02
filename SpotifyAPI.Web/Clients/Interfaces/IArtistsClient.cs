b21ad36f25b28c7609feeca46858973695d3c9a5
XY Wang <wxy11787@foxmail.com>
2022-12-02 23:24:27 +0800

    Add ArtistRequest, ArtistsRelatedArtistsRequest

--- SpotifyAPI.Web/Clients/Interfaces/IArtistsClient.cs
+++ SpotifyAPI.Web/Clients/Interfaces/IArtistsClient.cs
@@ -32,6 +32,18 @@ namespace SpotifyAPI.Web
     Task<FullArtist> Get(string artistId, CancellationToken cancel = default);
 
     /// <summary>
+    /// Get Spotify catalog information for a single artist identified by their unique Spotify ID.
+    /// </summary>
+    /// <param name="artistId">The Spotify ID of the artist.</param>
+    /// <param name="cancel">The cancellation-token to allow to cancel the request.</param>
+    /// <remarks>
+    /// https://developer.spotify.com/documentation/web-api/reference-beta/#endpoint-get-an-artist
+    /// </remarks>
+    /// <returns></returns>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1716")]
+    Task<FullArtist> Get(string artistId, ArtistRequest request, CancellationToken cancel = default);
+
+    /// <summary>
     /// Get Spotify catalog information about an artist’s albums.
     /// Optional parameters can be specified in the query string to filter and sort the response.
     /// </summary>
@@ -81,5 +93,17 @@ namespace SpotifyAPI.Web
     /// </remarks>
     /// <returns></returns>
     Task<ArtistsRelatedArtistsResponse> GetRelatedArtists(string artistId, CancellationToken cancel = default);
+
+    /// <summary>
+    /// Get Spotify catalog information about artists similar to a given artist.
+    /// Similarity is based on analysis of the Spotify community’s listening history.
+    /// </summary>
+    /// <param name="artistId">The Spotify ID for the artist</param>
+    /// <param name="cancel">The cancellation-token to allow to cancel the request.</param>
+    /// <remarks>
+    /// https://developer.spotify.com/documentation/web-api/reference-beta/#endpoint-get-an-artists-related-artists
+    /// </remarks>
+    /// <returns></returns>
+    Task<ArtistsRelatedArtistsResponse> GetRelatedArtists(string artistId, ArtistsRelatedArtistsRequest request, CancellationToken cancel = default);
   }
 }
