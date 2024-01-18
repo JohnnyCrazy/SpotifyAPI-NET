5722d05c9aa9a01d2e2e905dfd26481d7a92c91b
Noel Griffin <griffin.noel1@gmail.com>
2024-01-18 13:55:05 +0000

    Added Tests.

--- SpotifyAPI.Web.Tests/Clients/UserProfileClientTest.cs
+++ SpotifyAPI.Web.Tests/Clients/UserProfileClientTest.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Moq;
 using NUnit.Framework;
 using SpotifyAPI.Web.Http;
+using SpotifyAPI.Web.Models.Request;
 
 namespace SpotifyAPI.Web
 {
@@ -30,6 +31,30 @@ namespace SpotifyAPI.Web
       await client.Get(userId);
 
       api.Verify(a => a.Get<PublicUser>(SpotifyUrls.User(userId), It.IsAny<CancellationToken>()), Times.Once);
+    }
+    [Test]
+    public async Task GetTopTracks()
+    {
+
+      var request = new UsersTopItemsRequest(TimeRange.LongTerm);
+      var api = new Mock<IAPIConnector>();
+      var client = new UserProfileClient(api.Object);
+
+      var res =  await client.GetTopTracks(request);
+
+      api.Verify(a => a.Get<UsersTopTracksResponse>(SpotifyUrls.TopTracks(), request.BuildQueryParams(), It.IsAny<CancellationToken>()), Times.Once);
+    }    
+    [Test]
+    public async Task GetTopArtists()
+    {
+
+      var request = new UsersTopItemsRequest(TimeRange.LongTerm);
+      var api = new Mock<IAPIConnector>();
+      var client = new UserProfileClient(api.Object);
+
+      await client.GetTopArtists(request);
+
+      api.Verify(a => a.Get<UsersTopArtistsResponse>(SpotifyUrls.TopArtists(),request.BuildQueryParams(), It.IsAny<CancellationToken>()), Times.Once);
     }
   }
 }
