e55a0f4c9a480eb97c1f2bcff0574e7e02fb800c
Lewis-Fam <54489901+Lewis-Fam@users.noreply.github.com>
2024-01-26 09:10:31 -0700

    Fixed complier warning CA1305

--- SpotifyAPI.Web/Models/Converters/DoubleToIntConverter.cs
+++ SpotifyAPI.Web/Models/Converters/DoubleToIntConverter.cs
@@ -1,19 +1,21 @@
 using System;
+using System.Globalization;
+
 using Newtonsoft.Json;
 
 namespace SpotifyAPI.Web
 {
   public class DoubleToIntConverter : JsonConverter<int>
   {
-    public override void WriteJson(JsonWriter writer, int value, JsonSerializer serializer)
+    public override void WriteJson(JsonWriter? writer, int value, JsonSerializer serializer)
     {
-      writer.WriteValue(value);
+      writer?.WriteValue(value);
     }
 
-    public override int ReadJson(JsonReader reader, Type objectType, int existingValue, bool hasExistingValue,
+    public override int ReadJson(JsonReader? reader, Type objectType, int existingValue, bool hasExistingValue,
       JsonSerializer serializer)
     {
-      return Convert.ToInt32(reader.Value);
+      return reader != null ? Convert.ToInt32(reader.Value, CultureInfo.InvariantCulture) : 0;
     }
   }
 }
