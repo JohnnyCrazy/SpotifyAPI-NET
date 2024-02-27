b74f5c2148ac453b93c0112fc29b9f6b5551f527
Farkas Olivér <oli70963@gmail.com>
2024-02-27 21:29:20 +0100

    Update RecommendationsResponse to match Spotify API (#950)

--- SpotifyAPI.Web/Models/Response/RecommendationsResponse.cs
+++ SpotifyAPI.Web/Models/Response/RecommendationsResponse.cs
@@ -5,7 +5,7 @@ namespace SpotifyAPI.Web
   public class RecommendationsResponse
   {
     public List<RecommendationSeed> Seeds { get; set; } = default!;
-    public List<SimpleTrack> Tracks { get; set; } = default!;
+    public List<FullTrack> Tracks { get; set; } = default!;
   }
 }
 
