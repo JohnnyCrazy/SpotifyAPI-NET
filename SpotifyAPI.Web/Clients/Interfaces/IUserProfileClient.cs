2d136bb498120015a383d2cc0a585e1278b981fb
Mihai Mureșan <mihaimuresan592003@gmail.com>
2024-12-12 12:06:20 +0200

    feat: make top items paginatable

--- SpotifyAPI.Web/Clients/Interfaces/IUserProfileClient.cs
+++ SpotifyAPI.Web/Clients/Interfaces/IUserProfileClient.cs
@@ -36,7 +36,7 @@ namespace SpotifyAPI.Web
     /// <param name="cancel">The cancellation-token to allow to cancel the request.</param>
     /// <remarks>https://developer.spotify.com/documentation/web-api/reference/get-users-top-artists-and-tracks</remarks>
     /// <exception cref="APIUnauthorizedException">Thrown if the client is not authenticated.</exception>
-    Task<UsersTopTracksResponse> GetTopTracks(UsersTopItemsRequest request, CancellationToken cancel = default);
+    Task<CursorPaging<FullTrack>> GetTopTracks(UsersTopItemsRequest request, CancellationToken cancel = default);
 
     /// <summary>
     ///   Get Top arsists for the current user
@@ -45,6 +45,6 @@ namespace SpotifyAPI.Web
     /// <param name="cancel">The cancellation-token to allow to cancel the request.</param>
     /// <remarks>https://developer.spotify.com/documentation/web-api/reference/get-users-top-artists-and-tracks</remarks>
     /// <exception cref="APIUnauthorizedException">Thrown if the client is not authenticated.</exception>
-    Task<UsersTopArtistsResponse> GetTopArtists(UsersTopItemsRequest request, CancellationToken cancel = default);
+    Task<CursorPaging<FullArtist>> GetTopArtists(UsersTopItemsRequest request, CancellationToken cancel = default);
   }
 }
