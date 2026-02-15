38a743f03fcaa4ed31cab48cf975a99cb8586d73
Jonas Dellinger <jonas@dellinger.dev>
2026-02-15 23:49:51 +0100

    chore: use `dotnet nuget push` instead of `nuget`

--- publish.sh
+++ publish.sh
@@ -6,16 +6,14 @@ echo "Publishing..."
 
 cd ./SpotifyAPI.Web
 dotnet pack -c Release SpotifyAPI.Web.csproj -p:PackageVersion="$RELEASE_VERSION"
-nuget push "./bin/Release/SpotifyAPI.Web.$RELEASE_VERSION.nupkg"\
-  -ApiKey "$NUGET_TOKEN"\
-  -NonInteractive\
-  -Source https://www.nuget.org/api/v2/package
+dotnet nuget push "./bin/Release/SpotifyAPI.Web.$RELEASE_VERSION.nupkg"\
+  -k "$NUGET_TOKEN"\
+  -s https://www.nuget.org/api/v2/package
 
 cd ../SpotifyAPI.Web.Auth
 dotnet pack -c Release SpotifyAPI.Web.Auth.csproj -p:PackageVersion="$RELEASE_VERSION"
-nuget push "./bin/Release/SpotifyAPI.Web.Auth.$RELEASE_VERSION.nupkg"\
-  -ApiKey "$NUGET_TOKEN"\
-  -NonInteractive\
-  -Source https://www.nuget.org/api/v2/package
+dotnet nuget push "./bin/Release/SpotifyAPI.Web.Auth.$RELEASE_VERSION.nupkg"\
+  -k "$NUGET_TOKEN"\
+  -s https://www.nuget.org/api/v2/package
 
 cd ..
